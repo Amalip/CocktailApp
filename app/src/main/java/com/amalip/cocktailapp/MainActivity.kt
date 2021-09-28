91dced98e837d5012ef1b6496cd4cacb57f847c9
Amauri Guzmán <amauri.guzman@dacodes.com.mx>
2021-09-28 18:43:27 -0500

    - BaseActivity Generated - CocktailsList and FavoritesList design terminated v1 - NavComponents enable - App File created

--- app/src/main/java/com/amalip/cocktailapp/MainActivity.kt
+++ app/src/main/java/com/amalip/cocktailapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.amalip.cocktailapp
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.ui.NavigationUI
+import com.amalip.cocktailapp.core.presentation.BaseActivity
+import com.amalip.cocktailapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun layoutId() = R.layout.activity_main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setBinding()
     }
+
+    override val fragmentContainer: FragmentContainerView
+        get() = binding.fcv
+
+    override fun setUpNavigation(navController: NavController) =
+        NavigationUI.setupWithNavController(binding.bnvMain, navController)
+
+    override fun showProgress(show: Boolean) {
+        binding.progressView.isVisible = show
+    }
+
+    override fun setBinding() {
+        binding = DataBindingUtil.setContentView(this, layoutId())
+    }
+
+
 }
---
+++ app/src/main/java/com/amalip/cocktailapp/presentation/cocktails/detail/CocktailViewModel.kt
@@ -0,0 +1,7 @@
+package com.amalip.cocktailapp.presentation.cocktails.detail
+
+import androidx.lifecycle.ViewModel
+
+class CocktailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
